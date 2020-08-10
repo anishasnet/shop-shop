b573ab77c7d57d833a577202601700644ec9b4cc
anishasnet <anishasnet@gmail.com>
2020-08-09 22:42:35 -0700

    app javascript includes success, provider, store

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import OrderHistory from "./pages/OrderHistory";
+import Success from './pages/Success'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -28,6 +31,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+          <Provider store={store}>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -35,8 +39,10 @@ function App() {
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
             <Route component={NoMatch} />
           </Switch>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
